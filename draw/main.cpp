7edddd5d831c28824b5e1f4c00778703716c41f3
Ruan Yi <iynaur87@sina.com>
2021-01-19 13:55:55 +0800

    win specific

--- draw/main.cpp
+++ draw/main.cpp
@@ -1,45 +1,64 @@
+
+#ifdef WIN32
+#define _HAS_STD_BYTE 0
+#include "windows.h"
+#include "fileapi.h"
+#include "dbghelp.h"
+#pragma comment( lib, "Dbghelp.lib" )
+
+#ifdef _DEBUG
+#define new new(_NORMAL_BLOCK, __FILE__, __LINE__)
+#endif
+
+void EnableMemLeakCheck()
+{
+    int tmpFlag = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
+    tmpFlag |= _CRTDBG_LEAK_CHECK_DF;
+    _CrtSetDbgFlag(tmpFlag);
+}
+
+int crashhandler(EXCEPTION_POINTERS *pexception) {
+    QDateTime curDateTime = QDateTime::currentDateTime();
+    QString timestamp = curDateTime.toString("yy_MM_dd_hh_mm_ss")+".dmp";
+    HANDLE hDumpFile = CreateFile(
+        (LPCWSTR)timestamp.utf16(),
+        GENERIC_WRITE,
+        0,
+        NULL,
+        CREATE_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL);
+    if (hDumpFile != INVALID_HANDLE_VALUE) {
+        MINIDUMP_EXCEPTION_INFORMATION dumpInfo;
+        dumpInfo.ExceptionPointers = pexception;
+        dumpInfo.ThreadId = GetCurrentThreadId();
+        dumpInfo.ClientPointers = true;
+        MiniDumpWriteDump(
+            GetCurrentProcess(),
+            GetCurrentProcessId(),
+            hDumpFile,
+            MiniDumpNormal,
+            &dumpInfo,
+            NULL,
+            NULL);
+    }
+    return 0;
+}
+#endif
+
 #include "mainwindow.h"
 #include <QApplication>
 #include <string>
 #include"stable.h"
-//#include "windows.h"
-//#include "fileapi.h"
-//#include "dbghelp.h"
-
-//int crashhandler(EXCEPTION_POINTERS *pexception) {
-//	QDateTime curDateTime = QDateTime::currentDateTime();
-//	QString timestamp = curDateTime.toString("yy_MM_dd_hh_mm_ss")+".dmp";
-//	HANDLE hDumpFile = CreateFile(
-//		(LPCWSTR)timestamp.utf16(),
-//		GENERIC_WRITE,
-//		0,
-//		NULL,
-//		CREATE_ALWAYS,
-//		FILE_ATTRIBUTE_NORMAL,
-//		NULL);
-//	if (hDumpFile != INVALID_HANDLE_VALUE) {
-//		MINIDUMP_EXCEPTION_INFORMATION dumpInfo;
-//		dumpInfo.ExceptionPointers = pexception;
-//		dumpInfo.ThreadId = GetCurrentThreadId();
-//		dumpInfo.ClientPointers = true;
-//		MiniDumpWriteDump(
-//			GetCurrentProcess(),
-//			GetCurrentProcessId(),
-//			hDumpFile,
-//			MiniDumpNormal,
-//			&dumpInfo,
-//			NULL,
-//			NULL);
-//	}
-//	return 0;
-//}
 
 int main(int argc, char *argv[])
 {
-    //EnableMemLeakCheck();
+#ifdef WIN32
+    EnableMemLeakCheck();
+    _CrtSetBreakAlloc(30923);
+    SetUnhandledExceptionFilter((LPTOP_LEVEL_EXCEPTION_FILTER)crashhandler);
+#endif
     QApplication a(argc, argv);
-
-    //SetUnhandledExceptionFilter((LPTOP_LEVEL_EXCEPTION_FILTER)crashhandler);
 
     QTranslator translator;
     if (translator.load( QLatin1String("draw_zh_cn.qm"),
